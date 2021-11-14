b04a4684aa5de52f0271f25b2bf8cb3d4ae4d0ae
jerry <aafulin@gmail.com>
2021-11-14 10:12:30 +0800

    reformat code

--- TaxCalculationTests/FakeRepository.cs
+++ TaxCalculationTests/FakeRepository.cs
@@ -14,27 +14,32 @@ namespace TaxCalculationTests
                     Lower = 0m,
                     Upper = 540000m,
                     Rate = 0.05m,
-                }, new TaxRate()
+                },
+                new TaxRate()
                 {
                     Lower = 540000,
                     Upper = 1210000,
                     Rate = 0.12m,
-                }, new TaxRate()
+                },
+                new TaxRate()
                 {
                     Lower = 1210000,
                     Upper = 2420000,
                     Rate = 0.2m,
-                }, new TaxRate()
+                },
+                new TaxRate()
                 {
                     Lower = 2420000,
                     Upper = 4530000,
                     Rate = 0.3m,
-                }, new TaxRate()
+                },
+                new TaxRate()
                 {
                     Lower = 4530000,
                     Upper = 10310000,
                     Rate = 0.4m,
-                }, new TaxRate()
+                },
+                new TaxRate()
                 {
                     Lower = 10310000,
                     Upper = decimal.MaxValue,
